9ec72ba05afdd514f2e0772aa3c3ff934a387fba
Nick Kearns <nickkearns133@gmail.com>
2023-12-03 16:32:45 -0500

    Initial Commit

---
+++ TWC Scheduler/TWC_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWC_SchedulerApp.swift
+//  TWC Scheduler
+//
+//  Created by Nick Kearns on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TWC_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
